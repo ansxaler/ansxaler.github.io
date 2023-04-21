a64b1b63be4e539e3a0b5677d64aed3477190be8
Jeremy Thomas <bbxdesign@gmail.com>
2023-04-21 18:50:55 +0100

    Add scrolling cat

--- gsap.js
+++ gsap.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const $html = document.documentElement;
+  const $cat = document.getElementById("cat");
+  const $overlay = document.getElementById("overlay");
+
+  const darkenBackground = () => {
+    const howFarDown =
+      window.pageYOffset / ($html.scrollHeight - window.innerHeight);
+    const factor = howFarDown.toFixed(2);
+
+    $cat.style.setProperty("--x", factor);
+    $overlay.style.opacity = factor;
+  };
+
+  window.addEventListener("scroll", () => {
+    darkenBackground();
+  });
+
+  darkenBackground();
+
   gsap.registerPlugin(ScrollTrigger);
 
   const $messages = document.querySelectorAll(".message");
